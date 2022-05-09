f63a7808455e8c5518e1117ba943d034e0e512b0
arsalan khan <akhan492@gmail.com>
2022-05-09 13:55:11 +0530

    todo app with animation

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import "./App.css";
+import Slide from 'react-reveal/Slide';
+import React, { useState, useEffect } from "react";
+const getLocalItem = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(list);
+  }
+};
+const ClassList = [
+  "list-group-item-primary",
+  "list-group-item-secondary",
+  "list-group-item-success",
+  "list-group-item-danger",
+  "list-group-item-warning"
+];
+export default function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([getLocalItem()]);
+  const addtodo = () => {
+    setInput("");
+    if (input) {
+      setTodos((oldtodo) => [...oldtodo, input]);
+    }
+    const random = Math.floor(Math.random() * 5);
+    console.log(random);
+    const nodeList = document.querySelector(".list-group").lastElementChild;
+    nodeList.classList.add(ClassList[random]);
+  };
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(todos));
+  }, [todos]);
+  const deleteTodo = (ID) => {
+    const newTodos = todos.filter((i, id) => id !== ID);
+    setTodos(newTodos);
+  };
+  return (
+    <div className="App">
+      <div className="todos">
+        <h1>Todo list </h1>
+        <input
+          type="text"
+          value={input}
+          placeholder="add todo"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="addbtn" onClick={addtodo}>
+          addtodo
+        </button>
+        <ul className="list-group">
+          <li style={{ textAlign: "center" }}>add todo </li>
+          {todos.map((i, id) => (
+            <>
+              <Slide top>
+              <li id="li" key={id} className="list-group-item">
+                {i}{" "}
+                <button className="x" onClick={() => deleteTodo(id)}>
+                  <i className="fa-solid fa-trash" />
+                </button>
+                <i
+                  className="fa-solid fa-check"
+                  style={{ fontSize: "24px" }}
+                  onClick={() => {
+                    document.getElementById("li").classList.toggle("mark");
+                  }}
+                />
+              </li>
+              </Slide>
+            </>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );}
